5cfe0cfb3dcd7cb59ad24063b1c6891ae9795c84
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-04-02 12:55:40 -0300

    3 commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.mysite, name='mysite'), 
+    path('', views.list_location, name='list-location'),
+    path('form-client/', views.form_client, name='client-create'),
+     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from myapp.models import Immobile
+from myapp.forms import ClientForm
 
-# Create your views here.
-def mysite(request):
-    return render(request, 'index.html')
+
+def list_location(request):
+    immobiles = Immobile.objects.filter(is_locate=False)
+    context = {
+        'immobiles': immobiles
+    }
+    return render(request, 'list-location.html', context)
+
+def form_client(request):
+    form = ClientForm()
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list-location')
+    return render(request, 'form-client.html', {'form':form})
